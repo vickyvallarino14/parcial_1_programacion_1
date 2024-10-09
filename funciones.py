81305dc5cbeb73429ce9594f6c7085fb83d30a0d
victoria vallarino <vallarinovictoria14@gmail.com>
2024-10-09 09:59:39 -0300

    funcion menu

---
+++ funciones.py
@@ -0,0 +1,12 @@
+def menu():
+    print("""Menu Principal:
+    1.Cargar Pacientes
+    2.Mostrar todos los pacientes
+    3.Buscar pacientes por número de Historia Clinica
+    4.Ordenar pacientes por número de historia clínica
+    5.Mostrar paciente con más días de internación
+    6.Mostrar paciente con menos días de internación
+    7.Cantidad de pacientes con más de 5 días de internación
+    8.Promedio de días de internación de todos los pacientes
+    9.Salir    
+    """) 
---
+++ parcial.py
@@ -0,0 +1,27 @@
+from funciones import *
+
+seleccion=0
+while seleccion !="9":
+    menu()
+    seleccion=input("Ingrese su opcion: ")
+    match seleccion:
+        case "1": 
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4": 
+            pass
+        case "5":
+            pass
+        case "6": 
+            pass
+        case "7":
+            pass
+        case "8":
+            pass
+        case "9":
+            pass
+        case _:
+            print("error")
